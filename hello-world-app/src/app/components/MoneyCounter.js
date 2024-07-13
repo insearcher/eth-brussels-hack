ef988deb1ff4c0db86a8f80bb8ec9aef9d288c25
Maksim Frolov <mpfrolovstud@gmail.com>
2024-07-13 15:43:56 +0200

    Reworked for TS

--- hello-world-app/src/app/components/MoneyCounter.js
+++
@@ -1,9 +0,0 @@
-import styles from '../creator/[id]/page.module.css';
-
-export default function MoneyCounter({ money }) {
-  return (
-    <div className={styles.moneyCounter}>
-      <span>Money: {money}</span>
-    </div>
-  );
-}
