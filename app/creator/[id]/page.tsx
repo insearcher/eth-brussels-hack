50856e60464c9ce76758b44a331663e4406d9ffc
Maksim Frolov <mpfrolovstud@gmail.com>
2024-07-14 05:53:12 +0200

    Update design

--- app/creator/[id]/page.tsx
+++ app/creator/[id]/page.tsx
@@ -16,7 +16,7 @@ const items = [
 ];
 
 const creators = {
-  1: 'Wildways'
+  1: 'SEREBRO'
 }
 
 export default function Main() {
@@ -102,23 +102,23 @@ const handleStake = () => {
       <div className="flex flex-col items-center justify-center text-center">
         <div className="mb-6">
           <div className="inline-flex items-center justify-center">
-            <img src="/logo.png" alt="logo"/>
+            <img src="/images/serebro.png" alt="logo"/>
           </div>
         </div>
-        <h1 className="text-4xl font-bold mb-4">Creator {creatorName}</h1>
+        <h1 className="text-4xl font-bold mb-10">{creatorName}</h1>
 
         <DynamicWidget />
 
         {isMounted && isLoggedIn && (
           <>
             {/* Stake input and button */}
-            <div className="flex flex-row items-center mb-6">
+            <div className="flex flex-row items-center">
               <input
                 type="number"
                 step="0.0001"
                 value={stakeAmount}
                 onChange={(e) => setStakeAmount(e.target.value)}
-                className="bg-gray-800 text-white font-bold py-2 px-4 rounded mr-4"
+                className="bg-gray-800 text-white font-bold py-2 px-4 rounded mr-4 mt-4"
                 min="0.0001"
               />
               <button
@@ -130,28 +130,32 @@ const handleStake = () => {
             </div>
 
             {/* Creator points counter */}
-            <div className="text-xl font-bold mb-6">
+            <div className="text-xl font-bold mt-2">
               Creator points: {creatorPoints}
             </div>
 
             {/* List of items */}
             <div className="mt-10 space-y-4">
-              {items.map((item, index) => (
-                <div key={index} className="bg-gray-800 p-4 rounded-lg shadow-lg w-64 flex flex-row items-center">
-                  <div className="text-xl font-bold mb-2 mx-2">{item.name}</div>
-                  <div className="text-lg mb-4 mx-2">{item.price}</div>
-                  <button
-                    className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2 ${item.price > creatorPoints ? 'opacity-50 cursor-not-allowed' : ''}`}
-                    disabled={item.price > creatorPoints}
-                    onClick={() => handlePurchase(item)}
-                  >
-                    Purchase
-                  </button>
-                </div>
-              ))}
-            </div>
+  {items.map((item, index) => (
+    <div key={index} className="bg-gray-800 p-4 rounded-lg shadow-lg w-100 flex flex-row items-center justify-between">
+      <div className="text-xl font-bold mb-2 mx-2">{item.name}</div>
+      <div className="flex flex-row items-center">
+        <div className="text-lg mb-4 mx-2">{item.price}</div>
+        <button
+        className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2 ${item.price > creatorPoints ? 'opacity-50 cursor-not-allowed' : ''}`}
+        disabled={item.price > creatorPoints}
+        onClick={() => handlePurchase(item)}
+      >
+        Purchase
+      </button>
+      </div>
+    </div>
+  ))}
+</div>
           </>
         )}
+
+        <img className="mt-20" src="/images/stakes_logo.png" alt="logo"/>
       </div>
     </div>
   );
